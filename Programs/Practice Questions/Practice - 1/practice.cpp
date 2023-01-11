9ed91826c04014e0f33e9b0fd1dfb72db05b01bb
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2023-01-11 18:45:05 +0530

    Feat: Practice Questions

---
+++ Programs/Practice Questions/Practice - 1/practice.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // 1.
+
+    // int n;
+    // cin >> n;
+
+    // int product = 1;
+    // int sum = 0;
+
+    // while (n)
+    // {
+    //     product = product * (n % 10);
+    //     sum = sum + (n % 10);
+    //     n = n / 10;
+    // }
+
+    // cout << product - sum;
+
+    // 2.
+
+    // int n;
+    // cin >> n;
+    // int hammingWeight = 0;
+
+    // while (n)
+    // {
+    //     if (n % 10 != 0)
+    //     {
+    //         hammingWeight += 1;
+    //     }
+    //     n = n / 10;
+    // }
+
+    // cout << hammingWeight;
+
+    return 0;
+}
