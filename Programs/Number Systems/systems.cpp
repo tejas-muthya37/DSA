f2ceca610146e269c7632ec4baad0e3f7506fd93
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2023-01-12 12:23:36 +0530

    Feat: Number systems

---
+++ Programs/Number Systems/systems.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+    // 1. Decimal to bonary
+
+    // int n;
+    // cin >> n;
+    // int ans = 0;
+    // int i = 0;
+
+    // while (n != 0)
+    // {
+    //     int bit = n & 1;
+    //     ans = (bit * round(pow(10, i)) + ans);
+    //     n = n >> 1;
+    //     i++;
+    // }
+
+    // 2. Binary to decimal
+
+    // int n;
+    // cin >> n;
+
+    // int ans = 0;
+    // int i = 0;
+
+    // while (n != 0)
+    // {
+    //     int bit = n % 10;
+    //     if (bit)
+    //         ans = ans + round(pow(2, i));
+    //     i++;
+    //     n = n / 10;
+    // }
+
+    // cout << ans;
+}
